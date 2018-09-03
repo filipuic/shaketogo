c5f9c60135b0e57f522c2b981e880fb85e79e4e4
Filip Luchianenco <filip.luchianenco@uicentric.com>
2018-09-03 10:29:36 -0500

    initial commit

---
+++ app/src/main/java/engineer/filip/shaketogo/MainActivity.java
@@ -0,0 +1,13 @@
+package engineer.filip.shaketogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
